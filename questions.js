3743834703d29a6962c693f7dcbc2379691aec8a
chennamsettypoojitha <chpoojitha777@gmail.com>
2023-10-01 17:04:09 +0530

    commit

---
+++ questions.js
@@ -0,0 +1,118 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C.Hyper Text Markup Language",
+        options: [
+            "A.Hyper Type Multi Language",
+            "B.Hyper Text Multiple Language",
+            "C.Hyper Text Markup Language",
+            "D.Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following process scheduling algorithm may lead to starvation?",
+        answer: "B.Round Robin",
+        options: [
+            "A.FIFO",
+            "B.Round Robin",
+            "C.Shortest Job Next",
+            "D.None of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is called when a function is defined inside a class?",
+        answer: "D.Method",
+        options: [
+            "A.Module",
+            "B.Class",
+            "C.Another Function",
+            "D.Method"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does CSS stand for?",
+        answer: "A.Cascading Style Sheet",
+        options: [
+            "A.Cascading Style Sheet",
+            "B.Cute Style Sheet",
+            "C.Computer Style Sheet",
+            "D.Course Style Sheet"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A folder in windows computer can’t be made with the name",
+        answer: "A.con",
+        options: [
+            "A.con",
+            "B.can",
+            "C.make",
+            "D.mak"
+
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which one programming language is exclusively used for artificial intelligence?",
+        answer: "D.Prolog",
+        options: [
+            "A.C",
+            "B.Java",
+            "C.J2EE",
+            "D.Prolog"
+
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which of the following is not an operating system?",
+        answer: "A.C",
+        options: [
+            "A.C",
+            "B.Mac",
+            "C.DOS",
+            "D.Linux"
+
+        ]
+    },
+    {
+        numb: 8,
+        question: "1024 bit is equal to how many byte?",
+        answer: "B.128 Byte",
+        options: [
+            "A.Byte",
+            "B.128 Byte",
+            "C.32 Byte",
+            "D.64 Byte"
+
+        ]
+    },
+    {
+        numb: 9,
+        question: "How many layers in the TCP/IP protocol layers",
+        answer:"C.4",
+        options: [
+            "A.7",
+            "B.5",
+            "C.4",
+            "D.11"
+
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the binary value of ‘A’",
+        answer: "D.01000001",
+        options: [
+            "A.01000100",
+            "B.11000001",
+            "C.01100001",
+            "D.01000001"
+
+        ]
+    },
+];
